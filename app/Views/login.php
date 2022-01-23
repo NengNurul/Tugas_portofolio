13e16e441259b41483571ad28424ca3661762e34
yaden mustopa <brildent@gmail.com>
2022-01-23 19:18:35 +0700

    add login php

---
+++ app/Views/login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Password Manager</title>
+        <link rel="stylesheet" type="text/css" href="<?=base_url('/asset/css/style_login.css');?>">
+</head>
+<body>
+    <h2>Selamat datang di aplikasi Password Manager</h2>
+    <div class="section">
+        <div class="section-illustrator">
+            <div class="image"></div>
+        </div>
+        <div class="section-login" >
+            <div class="title_login">
+                <div colspan="2">Login Disini !</div>
+            </div>
+            <div class="wrap-input">
+                <div class="item-input">
+                    <div class="label" >Username : </div>
+                    <input type="text">
+                </div>
+                <div class="item-input">
+                    <div class="label">password : </div>
+                    <input type="password">
+                </div>
+            </div>
+        
+         
+            <div class="kirim">
+                <button>Kirim</button>
+            </div>
+        </div>
+    </div>
+
+  
+</body>
+</html>
