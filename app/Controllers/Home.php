0c90614f3eb3c9eeace0eb5338dd0d5daf96d535
yaden mustopa <brildent@gmail.com>
2022-01-23 19:17:53 +0700

    add home

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,16 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
+    
     public function index()
     {
-        return view('welcome_message');
+        $session = \Config\Services::session();
+        $loged_in = $session->get('loged_in');
+
+        if( ! $loged_in ){
+            return view('login');
+        }
+        
+        return view('index');
     }
 }
